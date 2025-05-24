e5560242eb9f0ac0d133d28562281fab4e9d6c5f
Eklak Alam <eklakalam420@gmail.com>
2025-05-24 20:57:37 +0530

    fdsf

--- app/layout.js
+++ app/layout.js
@@ -42,8 +42,8 @@ export default function RootLayout({ children }) {
       <head>
         {/* Favicon Links */}
         {/* <link rel="icon" href="/favicon.ico" sizes="any" /> */}
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" type="image/png" href="/favicon.png" /> */}
         <link rel="icon" href="/favicon-32x32.png" type="image/png" />
         <link rel="icon" href="/favicon-16x16.png" type="image/png" />
         <link rel="icon" href="/apple-touch-icon.png" type="image/png" />
